9d7da8ed98157bae751a965de1fb787d5bd1070e
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-08 21:15:37 +0000

    Added test and method for getAnswerCount (size of the ArrayList)

--- EightBall/src/main/java/EightBall.java
+++ EightBall/src/main/java/EightBall.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class EightBall {
+
+    private ArrayList<String> answers;
+
+    public EightBall(){
+        this.answers = new ArrayList<>();
+    }
+
+    public int getAnswerCount() {
+        return this.answers.size();
+    }
 }
---
+++ EightBall/src/test/java/EightBallTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EightBallTest {
+
+    EightBall eightBall;
+
+    @Before
+    public void before(){
+        eightBall = new EightBall();
+    }
+    @Test
+    public void answersArrayListHasLength(){
+        assertEquals(0, eightBall.getAnswerCount());
+    }
+}
